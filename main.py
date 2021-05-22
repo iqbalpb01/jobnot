28115a152f45a1f84c4dbbcb530de0ab76e7d86c
Abin Joseph <itsmeabjs@gmail.com>
2021-05-22 17:01:21 +0530

    Main

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from bs4 import BeautifulSoup as bs4
 import requests
 url = "https://in.linkedin.com/jobs/search?keywords="
 
-key = ["HTML","CSS","JAVASCRIPT","PYTHON","JAVA","GO","BASH"]
+key = ["HTML","CSS","JAVASCRIPT","PYTHON","JAVA","GO","BASH","data","software-developement-intern","networking-intern","machinelearning-datascience-intern"]
 
-python_url = url+"Java"
+python_url = url+"software-developement-intern"
 
 html = requests.get(python_url).text
 soup = bs4(html, "lxml")
@@ -14,4 +14,4 @@ for ultag in soup.find_all('ul', {'class': 'jobs-search__results-list'}):
         for atag in ultag.find_all('a'):
            links.append(atag.get('href'))
 
-print(python_url , len(links),links )
+print(python_url , len(links))
